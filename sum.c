ee8a7cff7b19ef5793746c921a05a546d3ee166b
shankarias <34618377+shankarias@users.noreply.github.com>
2018-01-12 11:52:18 +0530

    Update sum.c

--- sum.c
+++ sum.c
@@ -5,10 +5,12 @@ void main()
 	int i,num,sum=0;
 	printf("enter the number\n");
 	scanf("%d",&num);
-	for(i=0;i<=num;i++)
+	for(i=1;i<=num;i++)
+	{
 sum=sum+i;
-{
-	printf("the sum of %d natural numbers=%d",num,sum);
+	}
+	{
+	printf("the sum of natural numbers=%d",sum);
 	}
 
 }
