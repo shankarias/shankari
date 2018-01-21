14232e0ec7aee1045a83db24fa1fa9d0c9a8b93d
shankarias <34618377+shankarias@users.noreply.github.com>
2018-01-21 11:36:30 +0530

    Create primebtw.c

---
+++ primebtw.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main()
+{
+	int a,b,i,s;
+	printf("enter the number:");
+	scanf("%d%d",&a,&b);
+	while(a<b)
+	for(i=0;i<a/2;i++)
+	{
+		if(a%2==0)
+		{
+			s=1;
+			break;
+			
+		}
+		if(s==0)
+		{
+			printf("%d",a);
+		}
+		
+	return 0;
+		
+	}
+}
+	
+	
+	
+	
+	
