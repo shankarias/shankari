5007f1d22ffa20a8171b984ce7619446d780cef2
shankarias <34618377+shankarias@users.noreply.github.com>
2018-02-07 16:41:20 +0530

    Create 34COUNTLINES.C

---
+++ 34COUNTLINES.C
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int count=0;
+    char a[100];
+    scanf("%[^\n]",a);
+    for(int i=0;a[i]!=0;i++)
+    {
+    	if(a[i]=='.')
+            count++;    
+    }
+    printf("%d\n",count+1);
+    return 0;
+}
