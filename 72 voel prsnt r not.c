48b0a9831e19db7c9c6cbd51515eeed289eba860
shankarias <34618377+shankarias@users.noreply.github.com>
2018-03-12 16:47:26 +0530

    Create 72 voel prsnt r not.c

---
+++ 72 voel prsnt r not.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+    int i,count=0;
+	char a[100];
+    scanf("%s",a);	
+    for(i=0;a[i]!=0;i++)
+    {
+
+	    if (a[i]=='a'||a[i]=='e'||a[i]=='i'||a[i]=='o'||a[i]=='u'||a[i]=='A'||a[i]=='E'||a[i]=='I'||a[i]=='O'||a[i]=='U')
+		{
+	       count++;
+		}
+    }
+    if(count>0)
+    {
+	    	printf("yes");
+	 		}
+	else
+	{
+	printf("no");
+	}
+	return 0;
+}
