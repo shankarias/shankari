57833d0403c884cb0dffc04cfdabeb6a18b44ad0
shankarias <34618377+shankarias@users.noreply.github.com>
2018-03-19 16:57:21 +0530

    Create 88 lcm.c

---
+++ 88 lcm.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+	int a,b,i,gcd,l;
+	scanf("%d%d",&a,&b);	
+	for(i=1;i<=a&&i<=b;i++)
+	{
+		if(a%i==0&&b%i==0)
+			gcd=i;
+	}
+	l=(a*b)/gcd;
+	printf("%d",l);
+	return 0;
+}
