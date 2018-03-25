cc6926803f313245e3f89e149ef96eca6e0d624a
shankarias <34618377+shankarias@users.noreply.github.com>
2018-03-25 12:11:49 +0530

    Create 83 div mod.c

---
+++ 83 div mod.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,r1,c,d,r2;
+    scanf("%d %d",&a,&b);
+    scanf("%d  %d",&c,&d);
+    r1=a/b;
+    r2=c%d;
+    printf("\n%d",r1);
+    printf("\n%d",r2);
+}
